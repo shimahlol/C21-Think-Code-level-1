87b3009624352b5f3360e23325715f98e536a81a
jamiro.telgt <2048415@talnet.nl>
2021-11-26 16:49:00 +0100

    opdracht af?

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,4 @@
-#maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
-#elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+def top_games():
+    print("sims, sims 2, sims 3")
+def top_films():
+    print("cars, cars 2, cars 3")
